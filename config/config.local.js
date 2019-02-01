af89b48f301e0dce7097ec7d4ff6fd5aba71931b
fengpengyuan <fengpengyuan@live.com>
2019-02-02 01:47:34 +0800

    c测试提交

--- config/config.local.js
+++ config/config.local.js
@@ -6,7 +6,7 @@ module.exports = appInfo => {
     // use for cookie sign key, should change to your own and keep security
     config.keys = appInfo.name + '_1549037182088_4857';
 
-    //开发阶段取消csrf保护aaa
+    //开发阶段取消csrf保护
     config.security = {
         csrf: {
             enable: false,
