30b4cff6dc3b0e2671b89ae551033bf6ea2f9897
fengpengyuan <fengpengyuan@live.com>
2019-02-02 01:38:54 +0800

    a

---
+++ config/config.local.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = appInfo => {
+    const config = exports = {};
+
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1549037182088_4857';
+
+    config.security = {
+        csrf: {
+            enable: false,
+        }
+    };
+
+    // add your config here
+    config.middleware = [];
+
+    return config;
+};
